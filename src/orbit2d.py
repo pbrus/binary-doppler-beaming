d0fb48f1f119343cc7474cfa3244285cc736f58b
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-05-15 16:55:06 +0200

    Add docstrings to the Orbit2D class

--- src/orbit2d.py
+++ src/orbit2d.py
@@ -4,16 +4,34 @@ from astro_units import *
 
 
 class Orbit2D(UnitsConverter):
+    """Orbit2D represents the position and velocity vectors of any object
+    in binary system. For any given time t the: x(t), y(t), v_x(t), v_y(t)
+    are computed."""
 
     def __init__(self, first_mass, second_mass,
-                 semi_major_axis, eccentricity, perihelion_passage=0.0):
+                 semi_major_axis, eccentricity, periastron_passage=0.0):
+        """Set basic parameters of binary system.
+
+        Parameters
+        ----------
+        first_mass, second_mass : float
+            Star masses of binary system. Expressed in Sun mass unit.
+        semi_major_axis : float
+            Semi major axis of an orbit of star which must be calculated.
+            Expressed in meters.
+        eccentricity : float
+            A value of orbit's eccentricity between 0 and 1.
+        periastron_passage : float
+            A moment when a star passes periastron. Default = 0.0.
+        """
         self.first_mass = first_mass
         self.second_mass = second_mass
         self.semi_major_axis = semi_major_axis
         self.eccentricity = eccentricity
-        self.perihelion_passage = perihelion_passage
+        self.periastron_passage = periastron_passage
 
     def calculate_period(self):
+        """Calculate a period in seconds."""
         sum_mass = self.first_mass + self.second_mass
         self.period = sqrt(
             4*pow(pi,2)*pow(self.semi_major_axis, 3)
@@ -22,11 +40,13 @@ class Orbit2D(UnitsConverter):
         return self.period
 
     def calculate_mean_anomaly(self, time):
-        self.mean_anomaly = 2*pi*(time - self.perihelion_passage)/self.period
+        """Calculate a mean anomaly in radians."""
+        self.mean_anomaly = 2*pi*(time - self.periastron_passage)/self.period
 
         return self.mean_anomaly
 
     def calculate_eccentric_anomaly(self):
+        """Calculate an eccentric anomaly in radians."""
         self.solve_kepler_equation()
 
         return self.eccentric_anomaly
@@ -38,6 +58,7 @@ class Orbit2D(UnitsConverter):
         return self.eccentric_anomaly
 
     def calculate_true_anomaly(self):
+        """Calculate a true anomaly in radians."""
         x = sqrt(1 - self.eccentricity)*cos(0.5*self.eccentric_anomaly)
         y = sqrt(1 + self.eccentricity)*sin(0.5*self.eccentric_anomaly)
         self.true_anomaly = 2*atan2(y, x)
@@ -48,6 +69,7 @@ class Orbit2D(UnitsConverter):
         return self.true_anomaly
 
     def calculate_distance(self):
+        """Calculate distance in meters."""
         semilatus_rectum = self.semi_major_axis*(1 - pow(self.eccentricity,2))
         self.distance = semilatus_rectum/(1
             + self.eccentricity*cos(self.true_anomaly))
@@ -55,6 +77,7 @@ class Orbit2D(UnitsConverter):
         return self.distance
 
     def calculate_position(self):
+        """Calculate position tuple in meters."""
         x_coorindate = self.distance*cos(self.true_anomaly)
         y_coorindate = self.distance*sin(self.true_anomaly)
         self.position = x_coorindate, y_coorindate
