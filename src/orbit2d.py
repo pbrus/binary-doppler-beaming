51b5b5da2d5130e511f5887a8b8b9bf4f4cde58b
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-05-20 22:38:12 +0200

    Create a builder class for the Orbit2D class

--- src/orbit2d.py
+++ src/orbit2d.py
@@ -3,14 +3,12 @@ from scipy.optimize import fsolve
 from astro_units import *
 
 
-class Orbit2D(UnitsConverter):
-    """Orbit2D represents the position and velocity vectors of any object
-    in binary system. For any given time t the: x(t), y(t), v_x(t), v_y(t)
-    are computed."""
+class Orbit2DParameters:
+    """Orbit2DParameters is a builder class for the Orbit2D objects."""
 
     def __init__(self, first_mass, second_mass,
                  semi_major_axis, eccentricity, periastron_passage=0.0):
-        """Set basic parameters of binary system.
+        """Set basic parameters of binary system in 2D space.
 
         Parameters
         ----------
@@ -22,13 +20,26 @@ class Orbit2D(UnitsConverter):
         eccentricity : float
             A value of orbit's eccentricity between 0 and 1.
         periastron_passage : float
-            A moment when a star passes periastron. Default = 0.0.
+            A moment when a star passes periastron. Default = 0.
         """
         self.first_mass = first_mass
         self.second_mass = second_mass
         self.semi_major_axis = semi_major_axis
         self.eccentricity = eccentricity
         self.periastron_passage = periastron_passage
+
+
+class Orbit2D(UnitsConverter):
+    """Orbit2D represents the position and velocity vectors of any object
+    in binary system. For any given time t the: x(t), y(t), v_x(t), v_y(t)
+    are computed."""
+
+    def __init__(self, orbit2d):
+        self.first_mass = orbit2d.first_mass
+        self.second_mass = orbit2d.second_mass
+        self.semi_major_axis = orbit2d.semi_major_axis
+        self.eccentricity = orbit2d.eccentricity
+        self.periastron_passage = orbit2d.periastron_passage
 
     def calculate_period(self):
         """Calculate a period in seconds."""
