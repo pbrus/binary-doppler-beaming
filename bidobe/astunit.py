59c115c700ab64259e0990e0ef0b10a350f93a66
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-05-26 11:09:07 +0200

    Add new methods to the UnitsConverter class

--- bidobe/astunit.py
+++ bidobe/astunit.py
@@ -8,6 +8,7 @@ class UnitsConverter:
     SUN_MASS = 1.9884e30
     DAY = 86400
     AU = 1.49597e11
+    MINUTE = 60
 
     def convert_sun_mass_to_kg(self, mass):
         return mass*self.SUN_MASS
@@ -21,8 +22,20 @@ class UnitsConverter:
     def convert_sec_to_days(self, seconds):
         return seconds/self.DAY
 
+    def convert_min_to_sec(self, minutes):
+        return MINUTE*minutes
+
+    def convert_sec_to_min(self, seconds):
+        return secons/MINUTE
+
     def convert_au_to_m(self, au):
         return au*self.AU
 
     def convert_m_to_au(self, meters):
         return meters/self.AU
+
+    def convert_kmps_to_mps(self, speed):
+        return 1000.0*speed
+
+    def convert_mps_to_kmps(self, speed):
+        return speed/1000.0
