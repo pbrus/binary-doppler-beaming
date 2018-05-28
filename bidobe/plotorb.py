c011752898a788ebbf7e075f41f3a67a7f0b7160
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-05-28 12:37:19 +0200

    Add function to the plotorb.py file adjusting zoom for displayed projected orbits

--- bidobe/plotorb.py
+++ bidobe/plotorb.py
@@ -8,35 +8,63 @@ def plot_projected_orbits(orbit1, orbit2):
     ax.grid(color='gray', linestyle='--', linewidth=0.2)
     plt.xlabel('x (AU)')
     plt.ylabel('y (AU)')
-    xran, yran = choose_xy_ranges(orbit1, orbit2)
+    orbit = choose_greater_orbit(orbit1, orbit2)
+    arrow_length = calculate_arrow_length(orbit)
+    xran, yran = choose_xy_ranges(orbit, arrow_length)
+    plt.arrow(0, 0, arrow_length, 0, head_width=0.05*arrow_length,
+        head_length=0.1*arrow_length, fc='black', width=0.001*arrow_length)
+    plt.arrow(0, 0, 0, arrow_length, head_width=0.05*arrow_length,
+        head_length=0.1*arrow_length, fc='black', width=0.001*arrow_length)
     ax.set_xlim(xran)
     ax.set_ylim(yran)
-    dx = ax.get_xticks()[2] - ax.get_xticks()[0]
-    dy = ax.get_yticks()[2] - ax.get_yticks()[0]
-    plt.arrow(0, 0, dx, 0, head_width=0.05*dx, head_length=0.1*dx, fc='black')
-    plt.arrow(0, 0, 0, dy, head_width=0.05*dy, head_length=0.1*dy, fc='black')
+    ax.annotate("W", (arrow_length, 0.05*arrow_length))
+    ax.annotate("N", (0.05*arrow_length, arrow_length))
     plt.plot(orbit1[:,0], orbit1[:,1], 'r-', linewidth=0.5)
     plt.plot(orbit2[:,0], orbit2[:,1], 'b-', linewidth=0.5)
     plt.show()
 
-def choose_xy_ranges(orbit1, orbit2):
-    margin_scale = 0.1
+def choose_greater_orbit(orbit1, orbit2):
     x1_min = orbit1[:,0].min()
     x1_max = orbit1[:,0].max()
     x2_min = orbit2[:,0].min()
     x2_max = orbit2[:,0].max()
-    y1_min = orbit1[:,1].min()
-    y1_max = orbit1[:,1].max()
-    y2_min = orbit2[:,1].min()
-    y2_max = orbit2[:,1].max()
-    dx1 = x1_max - x1_min
-    dx2 = x2_max - x2_min
-    dy1 = y1_max - y1_min
-    dy2 = y2_max - y2_min
 
-    if dx1 > dx2:
-        return ((x1_min - margin_scale*dx1, x1_max + margin_scale*dx1),
-                (y1_min - margin_scale*dy1, y1_max + margin_scale*dy1))
+    if (x1_max - x1_min) > (x2_max - x2_min):
+        return orbit1
     else:
-        return ((x2_min - margin_scale*dx2, x2_max + margin_scale*dx2),
-                (y2_min - margin_scale*dy2, y2_max + margin_scale*dy2))
+        return orbit2
+
+def calculate_arrow_length(orbit):
+    arrow_length_scale = 0.2
+    x_min = orbit[:,0].min()
+    x_max = orbit[:,0].max()
+    y_min = orbit[:,1].min()
+    y_max = orbit[:,1].max()
+
+    minimum, maximum = choose_greater_range(x_min, x_max, y_min, y_max)
+    arrow_length = arrow_length_scale*(maximum - minimum)
+
+    return arrow_length
+
+def choose_xy_ranges(orbit, arrow_length):
+    x_min = orbit[:,0].min()
+    x_max = orbit[:,0].max()
+    y_min = orbit[:,1].min()
+    y_max = orbit[:,1].max()
+
+    minimum, maximum = choose_greater_range(x_min, x_max, y_min, y_max)
+    margin = 0.05*(maximum - minimum)
+
+    if arrow_length > x_max:
+        x_max = 1.2*arrow_length
+    if arrow_length > y_max:
+        y_max = 1.2*arrow_length
+
+    return (x_min - margin, x_max + margin), (y_min - margin, y_max + margin)
+
+def choose_greater_range(first_min, first_max, second_min, second_max):
+
+    if (first_max - first_min) > (second_max - second_min):
+        return first_min, first_max
+    else:
+        return second_min, second_max
