f6581388737af8c4d96010f07bf452a00574c671
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-06-07 22:22:16 +0200

    Add docstrings to the dobe.py file; set the convert_passband_to_frequency function as a method of the OrbitingObject class

--- bidobe/dobe.py
+++ bidobe/dobe.py
@@ -2,26 +2,47 @@ from math import pow, log10, e
 from bidobe.astunit import *
 
 
-def convert_passband_to_frequency(passband):
-    passbands_dict = {
-        "U" : 3.6e-7,
-        "B" : 4.4e-7,
-        "V" : 5.5e-7,
-        "I" : 9.0e-7
-    }
-
-    return UnitsConverter.LIGHT_SPEED/passbands_dict[passband]
-
-
 class OrbitingObject(UnitsConverter):
+    """OrbitingObject represents a single object in binary system and its basic
+    astrophysical parameters. Due to orbital motion an object's flux (doppler
+    beaming) varies with time.
+    """
 
     def __init__(self, distance, radius, temperature, passband):
+        """Set basic parameters of an object in a binary system.
+
+        Parameters
+        ----------
+        distance : float
+            Distance to the binary system in parsecs.
+        radius : float
+            Radius of the object in the Sun radius.
+        temperature : float
+            Temperature of the object in Kelvin.
+        passband : str
+            The passband which the object was observed in.
+            Avaible those in the convert_passband_to_frequency method.
+        """
         self.distance = self.convert_parsec_to_m(distance)
         self.radius = self.convert_sun_radius_to_m(radius)
         self.temperature = temperature
-        self.frequency = convert_passband_to_frequency(passband)
+        self.frequency = self.convert_passband_to_frequency(passband)
         self.calculate_stationary_flux()
         self.calculate_alpha_parameter()
+
+    def convert_passband_to_frequency(self, passband):
+        """
+        Change the name of a passband to the central frequency.
+        Available: U, B, V, I.
+        """
+        passbands_dict = {
+            "U" : 3.6e-7,
+            "B" : 4.4e-7,
+            "V" : 5.5e-7,
+            "I" : 9.0e-7
+        }
+
+        return self.LIGHT_SPEED/passbands_dict[passband]
 
     def calculate_stationary_flux(self):
         # For smaller temperatures than 5000K there is no sense
@@ -50,6 +71,10 @@ class OrbitingObject(UnitsConverter):
 
 
 def binary_brightness(object1, object2, zero_level=16.0):
+    """
+    Calculate brightness of a binary system taking into account doppler
+    beaming.
+    """
     doppler_flux = (object1.doppler_coefficient*object1.flux
                     + object2.doppler_coefficient*object2.flux)
 
