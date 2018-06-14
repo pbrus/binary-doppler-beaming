15b667f1d6032c5dd1d69df5311dd2169d43cdb9
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-06-14 11:21:39 +0200

    Add the following files: README.md requirements.txt setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='bidobe',
+      version='0.1.0',
+      description='Simulation of doppler beaming in binary systems',
+      author='Przemysław Bruś',
+      license='MIT',
+      url='https://github.com/pbrus/binary-doppler-beaming',
+      packages=['bidobe'],
+      keywords=['binary', 'doppler', 'beaming'],
+)
