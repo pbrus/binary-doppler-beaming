3bbde3459d76850cb1bfc99eea5d790dcd050b48
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-06-15 18:26:42 +0200

    Update __init__.py of the bidobe module

--- bidobe/__init__.py
+++ bidobe/__init__.py
@@ -1,1 +1,17 @@
+"""
+Bidobe
+======
+
+Bidobe is an acronym of BInary DOppler BEaming
+
+This package provides tools to:
+  1. solve a two body problem
+  2. calculate photometric doppler beaming
+  3. represent graphically determined parameters
+"""
+from . import astunit
+from . import dobe
+from . import orbit
+from . import plotorb
+
 __all__ = ["orbit", "astunit", "plotorb", "dobe"]
