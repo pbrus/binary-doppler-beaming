ca87cd6d2658826a5492b99d69d2f487fd408fcb
Przemysław Bruś <brus@astro.uni.wroc.pl>
2018-06-17 17:14:16 +0200

    Rename some functions in the plotorb.py file - apply the weak 'internal use' indicator (PEP008) to function names

--- bidobe/plotorb.py
+++ bidobe/plotorb.py
@@ -20,8 +20,8 @@ def plot_projected_orbits(orbit1, orbit2, xunit="m", yunit="m", filename=None):
         It should have the .eps extenstion. If None the image
         will be only displayed on a screen.
     """
-    figure = projected_orbits(orbit1, orbit2, xunit, yunit)
-    display_or_save_figure(figure, filename)
+    figure = _projected_orbits(orbit1, orbit2, xunit, yunit)
+    _display_or_save_figure(figure, filename)
 
 def animate_projected_orbits(orbit1, orbit2, xunit="m", yunit="m"):
     """
@@ -37,11 +37,11 @@ def animate_projected_orbits(orbit1, orbit2, xunit="m", yunit="m"):
         String which is x/y's label on an image.
         Default set in meters.
     """
-    figure = projected_orbits(orbit1, orbit2, xunit, yunit)
-    animation = anim_projected_orbits(figure, orbit1, orbit2)
-    display_or_save_figure(figure, None)
+    figure = _projected_orbits(orbit1, orbit2, xunit, yunit)
+    animation = _anim_projected_orbits(figure, orbit1, orbit2)
+    _display_or_save_figure(figure, None)
 
-def projected_orbits(orbit1, orbit2, xunit, yunit):
+def _projected_orbits(orbit1, orbit2, xunit, yunit):
     figure = plt.figure()
     ax = figure.add_subplot(111)
     ax.set_aspect('equal')
@@ -49,9 +49,9 @@ def projected_orbits(orbit1, orbit2, xunit, yunit):
     plt.xlabel('x (' + xunit + ')')
     plt.ylabel('y (' + yunit + ')')
     plt.title('Binary system')
-    init_xran, init_yran = choose_orbits_ranges(orbit1, orbit2)
-    arrow_length = calculate_arrow_length(init_xran, init_yran)
-    xran, yran = choose_xy_ranges(init_xran, init_yran, arrow_length)
+    init_xran, init_yran = _choose_orbits_ranges(orbit1, orbit2)
+    arrow_length = _calculate_arrow_length(init_xran, init_yran)
+    xran, yran = _choose_xy_ranges(init_xran, init_yran, arrow_length)
     plt.arrow(0, 0, arrow_length, 0, head_width=0.05*arrow_length,
         head_length=0.1*arrow_length, fc='black', width=0.001*arrow_length)
     plt.arrow(0, 0, 0, arrow_length, head_width=0.05*arrow_length,
@@ -66,10 +66,10 @@ def projected_orbits(orbit1, orbit2, xunit, yunit):
 
     return figure
 
-def anim_projected_orbits(figure, orbit1, orbit2):
+def _anim_projected_orbits(figure, orbit1, orbit2):
     line, = plt.plot(orbit1[:,0], orbit1[:,1], 'ko', animated=True)
 
-    def update_positions(i):
+    def _update_positions(i):
         x1 = orbit1[:,0][i]
         y1 = orbit1[:,1][i]
         x2 = orbit2[:,0][i]
@@ -78,12 +78,12 @@ def anim_projected_orbits(figure, orbit1, orbit2):
 
         return line,
 
-    animation = FuncAnimation(figure, update_positions,
+    animation = FuncAnimation(figure, _update_positions,
                               frames=range(len(orbit1)), interval=1, blit=True)
 
     return animation
 
-def choose_orbits_ranges(orbit1, orbit2):
+def _choose_orbits_ranges(orbit1, orbit2):
     x_min = min(orbit1[:,0].min(), orbit2[:,0].min())
     x_max = max(orbit1[:,0].max(), orbit2[:,0].max())
     y_min = min(orbit1[:,1].min(), orbit2[:,1].min())
@@ -91,25 +91,25 @@ def choose_orbits_ranges(orbit1, orbit2):
 
     return (x_min, x_max), (y_min, y_max)
 
-def calculate_arrow_length(xran, yran):
+def _calculate_arrow_length(xran, yran):
     arrow_length_scale = 0.2
     x_min = xran[0]
     x_max = xran[1]
     y_min = yran[0]
     y_max = yran[1]
 
-    minimum, maximum = choose_greater_range(x_min, x_max, y_min, y_max)
+    minimum, maximum = _choose_greater_range(x_min, x_max, y_min, y_max)
     arrow_length = arrow_length_scale*(maximum - minimum)
 
     return arrow_length
 
-def choose_xy_ranges(xran, yran, arrow_length):
+def _choose_xy_ranges(xran, yran, arrow_length):
     x_min = xran[0]
     x_max = xran[1]
     y_min = yran[0]
     y_max = yran[1]
 
-    minimum, maximum = choose_greater_range(x_min, x_max, y_min, y_max)
+    minimum, maximum = _choose_greater_range(x_min, x_max, y_min, y_max)
     margin = 0.1*(maximum - minimum)
 
     if arrow_length > x_max:
@@ -119,14 +119,14 @@ def choose_xy_ranges(xran, yran, arrow_length):
 
     return (x_min - margin, x_max + margin), (y_min - margin, y_max + margin)
 
-def choose_greater_range(first_min, first_max, second_min, second_max):
+def _choose_greater_range(first_min, first_max, second_min, second_max):
 
     if (first_max - first_min) > (second_max - second_min):
         return first_min, first_max
     else:
         return second_min, second_max
 
-def display_or_save_figure(figure, filename=None):
+def _display_or_save_figure(figure, filename=None):
     if filename:
         figure.savefig(filename, format="eps", bbox_inches=None)
     else:
@@ -155,8 +155,8 @@ def plot_radial_velocities(time, velocity1, velocity2,
         It should have the .eps extenstion. If None the image
         will be only displayed on a screen.
     """
-    figure = radial_velocities(time, velocity1, velocity2, xunit, yunit)
-    display_or_save_figure(figure, filename)
+    figure = _radial_velocities(time, velocity1, velocity2, xunit, yunit)
+    _display_or_save_figure(figure, filename)
 
 def animate_radial_velocities(time, velocity1, velocity2,
                               xunit="s", yunit="m/s"):
@@ -176,11 +176,11 @@ def animate_radial_velocities(time, velocity1, velocity2,
         String which is y's label on the image.
         Default set in meters per second.
     """
-    figure = radial_velocities(time, velocity1, velocity2, xunit, yunit)
-    animation = anim_radial_velocities(figure, time, velocity1, velocity2)
-    display_or_save_figure(figure, None)
+    figure = _radial_velocities(time, velocity1, velocity2, xunit, yunit)
+    animation = _anim_radial_velocities(figure, time, velocity1, velocity2)
+    _display_or_save_figure(figure, None)
 
-def radial_velocities(time, velocity1, velocity2, xunit="s", yunit="m/s"):
+def _radial_velocities(time, velocity1, velocity2, xunit="s", yunit="m/s"):
     figure = plt.figure()
     ax = figure.add_subplot(111)
     ax.grid(color='gray', linestyle='--', linewidth=0.2)
@@ -193,10 +193,10 @@ def radial_velocities(time, velocity1, velocity2, xunit="s", yunit="m/s"):
 
     return figure
 
-def anim_radial_velocities(figure, time, velocity1, velocity2):
+def _anim_radial_velocities(figure, time, velocity1, velocity2):
     line, = plt.plot(time, velocity1, 'ko', animated=True)
 
-    def current_velocities(i):
+    def _current_velocities(i):
         t = time[i]
         v1 = velocity1[i]
         v2 = velocity2[i]
@@ -204,7 +204,7 @@ def anim_radial_velocities(figure, time, velocity1, velocity2):
 
         return line,
 
-    animation = FuncAnimation(figure, current_velocities,
+    animation = FuncAnimation(figure, _current_velocities,
                     frames=range(len(time)), interval=1, blit=True)
 
     return animation
@@ -228,8 +228,8 @@ def plot_light_curve(time, magnitude, xunit="s", filename=None):
         It should have the .eps extenstion. If None the image
         will be only displayed on a screen.
     """
-    figure = light_curve(time, magnitude, xunit)
-    display_or_save_figure(figure, filename)
+    figure = _light_curve(time, magnitude, xunit)
+    _display_or_save_figure(figure, filename)
 
 def animate_light_curve(time, magnitude, xunit="s"):
     """
@@ -245,11 +245,11 @@ def animate_light_curve(time, magnitude, xunit="s"):
         String which is x's label on the image.
         Default set in seconds.
     """
-    figure = light_curve(time, magnitude, xunit)
-    animation = anim_light_curve(figure, time, magnitude)
-    display_or_save_figure(figure, None)
+    figure = _light_curve(time, magnitude, xunit)
+    animation = _anim_light_curve(figure, time, magnitude)
+    _display_or_save_figure(figure, None)
 
-def light_curve(time, magnitude, xunit):
+def _light_curve(time, magnitude, xunit):
     figure = plt.figure()
     ax = figure.add_subplot(111)
     ax.grid(color='gray', linestyle='--', linewidth=0.2)
@@ -262,17 +262,17 @@ def light_curve(time, magnitude, xunit):
 
     return figure
 
-def anim_light_curve(figure, time, magnitude):
+def _anim_light_curve(figure, time, magnitude):
     line, = plt.plot(time, magnitude, 'ko', animated=True)
 
-    def current_magnitude(i):
+    def _current_magnitude(i):
         t = time[i]
         mag = magnitude[i]
         line.set_data(t, mag)
 
         return line,
 
-    animation = FuncAnimation(figure, current_magnitude,
+    animation = FuncAnimation(figure, _current_magnitude,
                     frames=range(len(time)), interval=1, blit=True)
 
     return animation
